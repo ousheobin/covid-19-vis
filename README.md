0df7c7508785f7f4e2cca21a0bf248831d341c52
Shaobin.Ou <me@oushaobin.cn>
2020-03-14 18:47:24 +0800

    Update Data Source

--- README.md
+++ README.md
@@ -10,7 +10,6 @@
 - [国家统计局](http://data.stats.gov.cn/)
 - [国家卫生健康委](http://www.nhc.gov.cn/)
 - [BlankerL/DXY-COVID-19-Data](https://github.com/BlankerL/DXY-COVID-19-Data)
-- [澎湃新闻美数课 - 疫情数据汇总](https://github.com/839-Studio/Novel-Coronavirus-Updates)
 - [澎湃新闻美数课 - 病例数据汇总](https://github.com/839-Studio/Noval-Coronavirus-763-Cases)
 - [百度指数](http://index.baidu.com/)
 - [微博指数](https://data.weibo.com/index)
--- python/overall_data.py
+++ python/overall_data.py
@@ -2,37 +2,22 @@ import pandas as pd
 import numpy as np
 import datetime
 import json
+from os import path
 
-province_list = ['湖北','上海','北京','四川','广东','云南','天津','山东','河南','浙江','重庆','黑龙江','宁夏',
-                  '安徽','山西','广西','江苏','江西','河北','海南','湖南','福建','贵州','辽宁','内蒙古','吉林','新疆',
-                  '甘肃','陕西','青海','西藏','香港','澳门','台湾']
+province_list = ['湖北省','上海市','北京市','四川省','广东省','云南省','天津市','山东省','河南省','浙江省','重庆市','黑龙江省','宁夏回族自治区',
+                  '安徽省','山西省','广西壮族自治区','江苏省','江西省','河北省','海南省','湖南省','福建省','贵州省','辽宁省','内蒙古自治区','吉林省',
+                  '新疆维吾尔自治区','甘肃省','陕西省','青海省','西藏自治区','香港','澳门','台湾']
 
-today = datetime.date.today() 
-currTimeTs = pd.Timestamp(today)
+base_path = './DXY-COVID-19-Data/csv/'
 
-def cleanTimeError(date):
-    if date >= currTimeTs:
-        return currTimeTs
-    else:
-        return date
+cov_data = pd.read_csv(path.join(base_path,'DXYArea.csv'))
+cov_data['updateTime'] = pd.to_datetime(cov_data.updateTime).dt.date
+cov_data = cov_data[cov_data.provinceName.isin(province_list)]
+cov_data = cov_data.drop_duplicates(['provinceName','updateTime']).reset_index(drop=True)
+cov_data = cov_data[['provinceName','province_confirmedCount','province_curedCount','province_deadCount','updateTime']]
 
-path = './Novel-Coronavirus-Updates/Updates_NC.csv'
-try:
-    cov_data = pd.read_csv(path,encoding="utf8")
-except:
-    cov_data = pd.read_csv(path, encoding="gb2312")
-
-cov_data = cov_data.rename(columns={
-    '报道时间':'report_date','地区':'province','城市':'city','新增确诊':'confirmedIncr',
-    '新增出院':'curedIncr','新增死亡':'deathIncr','消息来源':'source','来源链接1':'link1',
-    '来源链接2':'link2','来源链接3':'link3'
-})
-cov_data = cov_data[cov_data.province.isin(province_list)]
-cov_data['report_date'] = pd.to_datetime(cov_data.report_date.apply(lambda x:u'2020年'+ x ),format=u'%Y年%m月%d日')
-cov_data['report_date'] = cov_data.report_date.apply(cleanTimeError)
-cov_data = cov_data[cov_data['report_date'].notnull()][['province','report_date','confirmedIncr','curedIncr','deathIncr']]
-cov_sum_province = cov_data.groupby(['province']).sum().reset_index()
-cov_sum_province = cov_sum_province.rename(columns={'confirmedIncr':'confirmed','curedIncr':'cured','deathIncr':'death'})
+cov_sum_province = cov_data.drop_duplicates(['provinceName'],keep='first').reset_index(drop=True).drop(['updateTime'],axis=1)
+cov_sum_province = cov_sum_province.rename(columns={'provinceName':'province','province_confirmedCount':'confirmed','province_curedCount':'cured','province_deadCount':'death'})
 
 stat = pd.read_csv('./externalData/stat.csv')
 stat = pd.merge(stat,cov_sum_province,on=['province'],how='right')
@@ -41,7 +26,7 @@ stat['healthStaffPer10w'] = ( stat.healthTechPersonNumber * 10000) / (stat.popul
 stat[['province','population','bedUsageRate','transportTotal','healthStaffPer10w','confirmed','cured','death']].fillna(-1).to_csv('./public/data/overall.stat.csv',index=False)
 
 dateObj = {
-    'thePaperData':cov_data.report_date.max().strftime('%Y-%m-%d'),
+    'thePaperData':cov_data.updateTime.max().strftime('%Y-%m-%d'),
     'theOverallData': '2018'
 }
 str = json.dumps(dateObj,ensure_ascii=False)
